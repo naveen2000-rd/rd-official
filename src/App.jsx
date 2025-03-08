bbadfe81ec26b5dff6f8b520e66dac95170b417a
naveen2000-rd <naveensankar0618@gmail.com>
2025-03-09 01:22:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import BlogSection from "./Component/BlogSection";
 import ContactForm from "./Component/Contact";
 import Footer from "./Component/Footer";
 import Header from "./Component/Header";
-import NiftySection from "./Component/NiftySection";
 import Services from "./Component/Services";
 import StatsSection from "./Component/StatsSection";
 
@@ -13,15 +12,16 @@ import StatsSection from "./Component/StatsSection";
 function App() {
   return (
     <>
-      <Header />
+    <ComingSoon></ComingSoon>
+      {/* <Header />
       <section id="banner"><Banner /></section>
       <section id="about"><AboutUs /></section>
       <section id="services"><Services /></section>
       <section id="stats"><StatsSection /></section>
       <section id="blog"><BlogSection /></section>
-      {/* <NiftySection></NiftySection> */}
+      
       <section id="contact"><ContactForm /></section>
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
