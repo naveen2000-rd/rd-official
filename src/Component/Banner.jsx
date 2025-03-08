c6da6b7dd18c09669cc7668c2864d2f10e28df17
naveen2000-rd <naveensankar0618@gmail.com>
2025-03-09 00:57:43 +0530

    Update Banner.jsx

--- src/Component/Banner.jsx
+++ src/Component/Banner.jsx
@@ -7,7 +7,8 @@ function Banner() {
   return (
     <div className="banner">
       <video autoPlay loop muted className="banner-video">
-        <source src="https://drive.google.com/file/d/1ts757YG05gTbUeA6Y89Y6ot5o4iemP3-/view?usp=drive_link" type="video/mp4" />
+       <source src="https://drive.google.com/uc?export=download&id=1ts757YG05gTbUeA6Y89Y6ot5o4iemP3-" type="video/mp4" />
+
       </video>
 
       <div className="overlay">
