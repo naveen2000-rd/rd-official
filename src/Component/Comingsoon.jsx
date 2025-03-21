5b1966e6b57e48bcb08fd80719434f77f85d1f75
naveen2000-rd <naveensankar0618@gmail.com>
2025-03-21 15:24:14 +0530

    Update Comingsoon.jsx

--- src/Component/Comingsoon.jsx
+++ src/Component/Comingsoon.jsx
@@ -1,16 +1,1 @@
-import React from "react";
-import "./ComingSoon.css";
 
-const ComingSoon = () => {
-  return (
-    <div className="container">
-      <div className="content">
-        <h1>Coming Soon</h1>
-        <p>We're working hard to bring something amazing. Stay tuned!</p>
-       
-      </div>
-    </div>
-  );
-};
-
-export default ComingSoon;
